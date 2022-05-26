ca0ca6c23cbad3123d1d0f5cc778e17384c04a51
Vern DeMille <vdemille@letsonboard.com>
2022-05-25 23:49:57 -0600

    Troubleshooting grep.

--- OnboardRS.ToDoIssues.Business/Utilities/Extensions/ShellHelper.cs
+++ OnboardRS.ToDoIssues.Business/Utilities/Extensions/ShellHelper.cs
@@ -4,7 +4,7 @@ namespace OnboardRS.ToDoIssues.Business.Utilities.Extensions;
 
 public static class ShellHelper
 {
-	public static async Task<string> RunBashCommandAsync (this string cmd, ILogger logger)
+	public static async Task<string> RunBashCommandAsync(this string cmd, ILogger logger)
 	{
 		var escapedArgs = cmd.Replace("\"", "\\\"");
 		var process = new Process
@@ -23,14 +23,17 @@ public static class ShellHelper
 
 		try
 		{
+			logger.LogInformation($"Running command {cmd}");
 			process.Start();
 			var output = await process.StandardOutput.ReadToEndAsync();
+			logger.LogInformation($"Result:\n{output}");
 			await process.WaitForExitAsync();
+
 			return output;
 		}
 		catch (Exception e)
 		{
-			logger.LogError(e, "Command {} failed", cmd);
+			logger.LogError(e, $"Command {cmd} failed");
 			var errorOutput = await process.StandardError.ReadToEndAsync();
 			throw new ApplicationException($"Bash error: {errorOutput}", e);
 		}
