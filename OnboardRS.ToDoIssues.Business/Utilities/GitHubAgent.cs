8f1ae781f661f505c1c2926d1b94cbdc845866af
Vern DeMille <vdemille@letsonboard.com>
2022-05-26 13:19:21 -0600

    Removing test items.

--- OnboardRS.ToDoIssues.Business/Utilities/GitHubAgent.cs
+++ OnboardRS.ToDoIssues.Business/Utilities/GitHubAgent.cs
@@ -90,12 +90,11 @@ public class GitHubAgent
 
 	public async Task<Issue> CreateGitHubIssueAsync(ToDoIssueModel toDoIssueModel)
 	{
-		// TODO [#30]:
 		var newIssue = new NewIssue(toDoIssueModel.Title)
 		{
 			Body = toDoIssueModel.Body
 		};
-		// TODO [#31]: Test new todo label.
+
 		foreach (var issueLabel in _config.IssueLabels)
 		{
 			newIssue.Labels.Add(issueLabel);
