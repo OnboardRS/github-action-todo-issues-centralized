4012d051af9f10f9f3d43e1cf36dbf887f3e3639
Vern DeMille <vdemille@letsonboard.com>
2022-06-01 20:18:49 -0600

    Restoring TODO marker post merge.

--- OnboardRS.ToDoIssues.Business/Utilities/Extensions/ToDoParserExtensions.cs
+++ OnboardRS.ToDoIssues.Business/Utilities/Extensions/ToDoParserExtensions.cs
@@ -4,9 +4,6 @@ namespace OnboardRS.ToDoIssues.Business.Utilities.Extensions;
 
 public static class ToDoParserExtensions
 {
-
-	//public const string TODO_REGULAR_EXPRESSION = @"^(\W+\s)" + ToDoConstants.TO_DO_MARKER + @"(?: \[([^\]\s]+)\])?:(.*)";
-
 	public const string TODO_REGULAR_EXPRESSION = $@"^(\W+\s){ToDoConstants.TO_DO_MARKER}(?: \[([^\]\s]+)\])?:(.*)";
 	private static readonly Regex Regex = new(TODO_REGULAR_EXPRESSION, RegexOptions.Multiline);
 
